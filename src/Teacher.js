a2e9e8e7c9a0eea84f2bf787ec4e5724f21dad3b
haroonahmed786 <Haroon65ahmed@gmail.com>
2022-09-24 09:24:58 -0700

    class & function

---
+++ src/Teacher.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Teacher extends Component{
+    render(){
+             return<h1>Hello {this.props.name}</h1>;
+    }
+}
+export default Teacher;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+import Student from "./Student";
+import Teacher from "./Teacher";
+
+ReactDOM.render(<Student name="Rahul"/>,document.getElementById("root"));
+
+ReactDOM.render(<Teacher name="Haroon"/>,document.getElementById("root"));
